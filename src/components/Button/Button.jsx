8df335e7c241ee053ebf6cb2f471f0d261a63ab0
Tatsiana Krautsova2 <Tatsiana_Krautsova2@epam.com>
2020-02-19 13:24:53 +0300

    feat: add onclick on button

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -3,13 +3,20 @@ import PropTypes from 'prop-types';
 
 import styles from './Button.scss';
 
-const Button = ({ children, purpose }) => (
-  <button className={`${styles.button} ${styles[purpose]}`} type="button">{children}</button>
+const Button = ({ children, purpose, clickHandler }) => (
+  <button
+    className={`${styles.button} ${styles[purpose]}`}
+    type="button"
+    onClick={clickHandler}
+  >
+    {children}
+  </button>
 );
 
 Button.propTypes = {
   children: PropTypes.string.isRequired,
   purpose: PropTypes.string.isRequired,
+  clickHandler: PropTypes.func.isRequired,
 };
 
 export default Button;
