ee8dea668af48d9ff6b4a0273b210124a7431aa0
Tatsiana Krautsova2 <Tatsiana_Krautsova2@epam.com>
2020-02-27 15:15:11 +0300

    feat: add footer

--- src/pages/MainPage/MainPage.jsx
+++ src/pages/MainPage/MainPage.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Header from '../../components/Header';
 import MoviesSection from '../../components/MoviesSection';
+import Footer from '../../components/Footer';
 
 const MainPage = () => (
   <div>
     <Header />
     <MoviesSection />
+    <Footer />
   </div>
 );
 
