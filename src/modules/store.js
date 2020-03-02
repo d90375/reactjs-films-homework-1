4344ecb2a109484e5f820951a5d4afb7d2368523
Tatsiana Krautsova2 <Tatsiana_Krautsova2@epam.com>
2020-03-02 13:37:19 +0300

    fix: fix lint error

--- src/modules/store.js
+++ src/modules/store.js
@@ -13,4 +13,5 @@ const rootReducer = combineReducers({
 });
 
 export default createStore(rootReducer, compose(applyMiddleware(thunk),
+/* eslint no-underscore-dangle: 0 */
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()));
