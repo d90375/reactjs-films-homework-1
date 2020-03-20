e118e83226186ccc108590d9d2ec39b26a63c502
tanyak1601 <50367908+tanyak1601@users.noreply.github.com>
2020-03-21 02:20:31 +0300

    Update routes.js

--- src/routes.js
+++ src/routes.js
@@ -6,18 +6,6 @@ const routes = [
     path: '/details/:id',
     component: MovieDetailsPage,
   },
-  /*  {
-    path: '/search/:query',
-    component: MainPage,
-  },
-  {
-    path: '/genre/:genreId',
-    component: MainPage,
-  },
-  {
-    path: '/:filter',
-    component: MainPage,
-  }, */
   {
     path: '/',
     component: MainPage,
